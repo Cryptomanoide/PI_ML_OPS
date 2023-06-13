c2baabde3b9f99146e5b51b6fc88ec6ed2ac0932
Jose Betancourt <80551934+Cryptomanoide@users.noreply.github.com>
2023-06-13 14:12:32 -0400

    Add files via upload

--- API.py
+++ API.py
@@ -2,113 +2,114 @@ from fastapi import FastAPI
 import uvicorn
 import pandas as pd
 
-# Cargar los dataset en un DataFrame
-merged_data = pd.read_csv(r'C:\Users\josgr\Desktop\PI_ML_OPS\Data transformada\movies.csv')
+# Cargar el DataFrame desde el archivo CSV
+df = pd.read_csv('C:/Users/josgr/Desktop/PI_ML_OPS/api_data.csv')
 
-
-# Crear una instancia de la aplicación FastAPI
+# Crear la instancia de FastAPI
 app = FastAPI()
 
-# Definir las funciones para los endpoints de la API
 
 @app.get('/cantidad_filmaciones_mes/{mes}')
 def cantidad_filmaciones_mes(mes: str):
-    # Filtrar las películas por mes y contar la cantidad
-    cantidad = len(merged_data[merged_data['release_date'].str[5:7] == mes])
+    # Filtra las películas por mes y cuenta la cantidad
+    cantidad = len(df[df['release_date'].str[5:7] == mes])
     return f"{cantidad} cantidad de películas fueron estrenadas en el mes de {mes}"
 
+
+
+
+# Endpoint para obtener la cantidad de filmaciones en un día específico
 @app.get('/cantidad_filmaciones_dia/{dia}')
 def cantidad_filmaciones_dia(dia: str):
-    # Filtrar las películas por día y contar la cantidad
-    cantidad = len(merged_data[merged_data['release_date'].str[8:10] == dia])
-    return f"{cantidad} cantidad de películas fueron estrenadas en el día {dia}"
+    dia = dia.lower()
+    cantidad = len(df[df['release_date'].str.endswith(f"-{dia}")])
+    return f"{cantidad} cantidad de películas fueron estrenadas en los días {dia.capitalize()}"
 
+# Endpoint para obtener el score de una película por su título
 @app.get('/score_titulo/{titulo}')
 def score_titulo(titulo: str):
-    # Filtrar la película por título
-    pelicula = merged_data[merged_data['title'] == titulo]
+    row = df[df['title'] == titulo]
+    if not row.empty:
+        return f"La película {row['title'].iloc[0]} fue estrenada en el año {row['release_year'].iloc[0]} con un score/popularidad de {row['popularity'].iloc[0]}"
+    else:
+        return f"No se encontró la película '{titulo}'"
 
-    if len(pelicula) == 0:
-        return f"No se encontró la película con el título {titulo}"
-
-    # Obtener el título, año de estreno y score
-    titulo = pelicula['title'].values[0]
-    anio_estreno = pelicula['release_date'].str[:4].values[0]
-    score = pelicula['popularity'].values[0]
-
-    return f"La película {titulo} fue estrenada en el año {anio_estreno} con un score/popularidad de {score}"
-
+# Endpoint para obtener la cantidad de votos y promedio de votaciones de una película
 @app.get('/votos_titulo/{titulo}')
 def votos_titulo(titulo: str):
-    # Filtrar la película por título
-    pelicula = merged_data[merged_data['title'] == titulo]
+    row = df[df['title'] == titulo]
+    if not row.empty:
+        votos = float(row['vote_count'].iloc[0])
+        promedio = float(row['vote_average'].iloc[0])
+        if votos >= 2000:
+            return f"La película {row['title'].iloc[0]} fue estrenada en el año {row['release_year'].iloc[0]}. La misma cuenta con un total de {votos} valoraciones, con un promedio de {promedio}"
+        else:
+            return f"La película {row['title'].iloc[0]} fue estrenada en el año {row['release_year'].iloc[0]}. La misma no cumple con la condición de tener al menos 2000 valoraciones."
+    else:
+        return f"No se encontró la película '{titulo}'"
+    
+@app.get('/get_actor/{nombre_actor}')
+def get_actor(nombre_actor: str):
+    # Rellenar los valores NaN en la columna 'actors' con una cadena vacía
+    df['actors'] = df['actors'].fillna('')
 
-    if len(pelicula) == 0:
-        return f"No se encontró la película con el título {titulo}"
+    # Filtrar el DataFrame por el nombre del actor
+    actor_df = df[df['actors'].str.contains(nombre_actor, case=False)]
 
-    # Obtener la cantidad de votos y el valor promedio de votaciones
-    votos_count = pelicula['vote_count'].values[0]
-    vote_average = pelicula['vote_average'].values[0]
+    # Crear una lista de resultados
+    results = []
+    for _, row in actor_df.iterrows():
+        result = {
+            'title': row['title'],
+            'release_date': row['release_date'],
+            'revenue': row['revenue'],
+            'budget': row['budget'],
+        }
+        results.append(result)
 
-    if votos_count < 2000:
-        return f"La película {titulo} no cumple con la cantidad mínima de votaciones (2000) y no se devuelve ningún valor."
+    return {'actor': nombre_actor, 'movies': results}
 
-    return f"La película {titulo} fue estrenada en el año {pelicula['release_date'].values[0][:4]}. La misma cuenta con un total de {votos_count} valoraciones, con un promedio de {vote_average}."
-
-@app.get("/actor/{nombre_actor}")
-def get_actor(nombre_actor):
-    # Filtrar el dataset "credits" por nombre de actor
-    actor_films = merged_data[merged_data['cast'].apply(lambda x: any(actor['name'] == nombre_actor and actor['job'] != 'Director' for actor in x))]
-
-    if len(actor_films) == 0:
-        return f"No se encontraron registros para el actor {nombre_actor}"
-
-    # Obtener la cantidad de películas y el promedio de retorno del actor
-    num_films = len(actor_films)
-    total_return = 0
-    for index, row in actor_films.iterrows():
-        movie_id = row['id']
-        movie_return = merged_data.loc[merged_data['id'] == movie_id, 'return'].values[0]
-        total_return += movie_return
-
-    average_return = total_return / num_films
-
-    return f"El actor {nombre_actor} ha participado en {num_films} filmaciones. Ha conseguido un retorno total de {total_return} con un promedio de {average_return} por filmación."
-
-@app.get("/director/{nombre_director}")
-def get_director(nombre_director):
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director: str):
     # Filtrar el dataset "credits" por nombre de director
-    director_films = merged_data[merged_data['crew'].apply(lambda x: any(crew['name'] == nombre_director and crew['job'] == 'Director' for crew in x))]
+    director_films = df['director']
 
     if len(director_films) == 0:
         return f"No se encontraron registros para el director {nombre_director}"
 
-    # Obtener el retorno total del director
-    director_return = 0
-    film_details = []
+    director_success = 0
+    films_info = []
 
     for index, row in director_films.iterrows():
         movie_id = row['id']
-        movie_return = merged_data.loc[merged_data['id'] == movie_id, 'return'].values[0]
-        director_return += movie_return
+        movie_info = df.loc[df['id'] == movie_id, ['title', 'release_date', 'return', 'budget', 'revenue']]
 
-        movie_title = merged_data.loc[merged_data['id'] == movie_id, 'title'].values[0]
-        release_date = merged_data.loc[merged_data['id'] == movie_id, 'release_date'].values[0]
-        budget = merged_data.loc[merged_data['id'] == movie_id, 'budget'].values[0]
-        revenue = merged_data.loc[merged_data['id'] == movie_id, 'revenue'].values[0]
+        if len(movie_info) > 0:
+            movie_title = movie_info['title'].values[0]
+            release_date = movie_info['release_date'].values[0]
+            movie_return = movie_info['return'].values[0]
+            movie_budget = movie_info['budget'].values[0]
+            movie_revenue = movie_info['revenue'].values[0]
 
-        film_details.append({
-            'title': movie_title,
-            'release_date': release_date,
-            'return': movie_return,
-            'budget': budget,
-            'revenue': revenue
-        })
+            director_success += movie_return
+            films_info.append((movie_title, release_date, movie_return, movie_budget, movie_revenue))
+
+    if len(films_info) == 0:
+        return f"No se encontraron registros de películas para el director {nombre_director}"
 
     return {
-        'director': nombre_director,
-        'success': director_return,
-        'film_details': film_details
+        "director_name": nombre_director,
+        "director_success": director_success,
+        "films_info": films_info
     }
 
+
+
+
+
+
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
